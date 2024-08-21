42d43c49df17ed64182647121403ed61bcbde8da
Fabio Barros <fabio.junior643@outlook.com>
2024-08-20 23:35:21 -0300

    initial config

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import * as Styles from "./App.styles";
+
+function App() {
+  return <Styles.constainer />;
+}
+
+export default App;
---
+++ src/GlobalStyle.ts
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+  html,
+  body,
+  #root {
+    padding: 0;
+    margin: 0;
+    width: 100vw;
+    height: 100vh;
+  }
+`;
